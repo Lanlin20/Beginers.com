e031fe7d486367deb45b2d4cb045d6131edb37f6
Lanlin20 <reubenshepherd2003@gmail.com>
2023-10-12 09:08:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+'''weight=float(input('WEIGHT:'))
+measure=str(input('K(kgs) or L(lbs):'))
+if weight=='K':
+    convert=weight / 0.45
+    print("Weight in Lbs is " +str(convert))
+else:
+    str(convert)=weight / 0.45
+    print("Weight in Kgs is " , +convert, +)'''
+##program 2 OO ORIENTED PROGRAMMING
+class dog:
+    def __int__(self,name):
+        self.name=name
+
+d=dog("tim")
+
+
